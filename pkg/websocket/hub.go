7615b823c4f13ffa798bc1710ec18fb38176dcbb
FKuiv <kuivfred@gmail.com>
2023-11-10 16:39:51 +0200

    Started creating client hub to manage ws connections

---
+++ pkg/websocket/hub.go
@@ -0,0 +1,66 @@
+package websocket
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Client struct {
+	conn *websocket.Conn
+	send chan []byte
+}
+
+type Hub struct {
+	clients    map[string]*Client
+	register   chan *Client
+	unregister chan *Client
+	broadcast  chan []byte
+	mutex      sync.Mutex
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[string]*Client),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		broadcast:  make(chan []byte),
+	}
+}
+
+func (c *Client) ID() string {
+	return fmt.Sprintf("%p", c)
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.mutex.Lock()
+			h.clients[client.ID()] = client
+			h.mutex.Unlock()
+			log.Printf("User %s registered. Total users: %d", client.ID(), len(h.clients))
+
+		case client := <-h.unregister:
+			h.mutex.Lock()
+			delete(h.clients, client.ID())
+			close(client.send)
+			h.mutex.Unlock()
+			log.Printf("User %s unregistered. Total users: %d", client.ID(), len(h.clients))
+
+		case message := <-h.broadcast:
+			h.mutex.Lock()
+			for _, client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client.ID())
+				}
+			}
+			h.mutex.Unlock()
+		}
+	}
+}
