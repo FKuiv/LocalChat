fb21cc73557c703f3bf767c26158008c9b1bfbfc
FKuiv <kuivfred@gmail.com>
2024-01-05 16:15:33 +0200

    updated logo component

--- frontend/src/components/ui/Logo.tsx
+++ frontend/src/components/ui/Logo.tsx
@@ -1,11 +1,13 @@
-import { Flex, Image, Title } from "@mantine/core";
+import { Avatar, Flex, Title } from "@mantine/core";
 import logo from "../../media/logo.png";
 
 const Logo = () => {
   return (
-    <Flex align="center" justify="center" style={{ flexGrow: 1 }}>
-      <Image h={40} w="auto" fit="contain" src={logo} />
-      <Title>Localchat</Title>
+    <Flex align="center" justify="center" gap={8} style={{ flexGrow: 1 }}>
+      <Avatar src={logo} alt="Localchat" radius="0" size="md" />
+      <Title order={1} pt={10}>
+        Localchat
+      </Title>
     </Flex>
   );
 };
