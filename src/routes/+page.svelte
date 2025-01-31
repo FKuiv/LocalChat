218926b820220f875be6b5d156aa0352768e268b
FKuiv <kuivfred@gmail.com>
2025-01-31 17:36:05 +0200

    Init new sveltekit project with Bun

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,2 @@
-<main>
-	<a href="/login">Login</a>
-</main>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,39 +1,11 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import { skeleton } from '@skeletonlabs/tw-plugin'
+import type { Config } from 'tailwindcss';
 
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
 	theme: {
-		extend: {},
+		extend: {}
 	},
-	plugins: [
-		forms,
-		typography,
-		skeleton({
-			themes: {
-				preset: [
-					{
-						name: 'gold-nouveau',
-						enhancements: true,
-					},
-					{
-						name: 'vintage',
-						enhancements: true,
-					},
-					{
-						name: 'modern',
-						enhancements: true,
-					},
-					{
-						name: 'wintry',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+
+	plugins: []
 } satisfies Config;
