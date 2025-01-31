e141b2dfa617249d3a165854a16aae708dc21cb5
FKuiv <kuivfred@gmail.com>
2025-01-31 18:49:39 +0200

    start converting to tailwind 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Input from '$lib/components/ui/input/input.svelte';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+</script>
+
+<div class="grid h-screen grid-cols-3 gap-0">
+	<aside>here are chats</aside>
+	<Separator orientation="vertical" class="w-5" />
+	<main class="w-full">
+		<Input placeholder="Type something" />
+	</main>
+</div>
