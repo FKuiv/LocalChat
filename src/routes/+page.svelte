4d77b923a5503f1f96f54e08e24a81fdb7f554a6
FKuiv <kuivfred@gmail.com>
2025-02-01 17:09:19 +0200

    Simple chat UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	// import Input from '$lib/components/ui/input/input.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
 </script>
 
-<div class="grid h-screen grid-cols-3 gap-0">
-	<aside>here are chats</aside>
-	<Separator orientation="vertical" class="w-5" />
-	<main class="w-full">
-		<!-- <Input placeholder="Type something" /> -->
+<div class="flex h-screen">
+	<aside class="flex w-1/4 justify-between">
+		<div class="grow p-4">here are chats</div>
+		<Separator orientation="vertical" />
+	</aside>
+	<main class="flex grow flex-col-reverse items-center p-4 pb-10">
+		<Input placeholder="Type something" />
 	</main>
 </div>
